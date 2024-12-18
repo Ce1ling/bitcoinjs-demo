a93f2ae985bf32e4d60d2d9a1e395b4f8f6c079b
l1en <2134045201@qq.com>
2024-12-18 17:10:45 +0800

    fix: deps & update src/index.ts

--- templates/package/src/index.ts
+++ templates/package/src/index.ts
@@ -1,5 +1,8 @@
+import { bitcoin } from '@bitcoinjs-demo/core'
+
 export const main = async () => {
-  console.log('Hello World')
+  console.log('Hello bitcoinjs-lib')
+  console.log('OP_RETURN:', bitcoin.opcodes.OP_RETURN)
 }
 
 main()
