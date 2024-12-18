dc0b5ab37e4ef9a850c3a5672700e9f2ed3756e1
l1en <2134045201@qq.com>
2024-12-18 17:20:21 +0800

    chore(templates): update package index file

--- templates/package/src/index.ts
+++ templates/package/src/index.ts
@@ -1,8 +1,4 @@
 import { bitcoin } from '@bitcoinjs-demo/core'
 
-export const main = async () => {
-  console.log('Hello bitcoinjs-lib')
-  console.log('OP_RETURN:', bitcoin.opcodes.OP_RETURN)
-}
-
-main()
+console.log('Hello bitcoinjs-lib')
+console.log('OP_RETURN:', bitcoin.opcodes.OP_RETURN)
